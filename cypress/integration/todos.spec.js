f6496c8860497cd3a5c326e2a98f3f4d1256a32d
brettshollenberger <brett.shollenberger@gmail.com>
2019-02-04 14:04:08 -0500

    Update to fixtures

--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,44 +1,31 @@
 describe('Todo Application', () => {
-    it('loads the page', () => {
-        cy.server()
-        cy.route('/api/todos', [
-          {
-            "id": 1,
-            "text": "Hello world",
-            "completed": false
-          },
-          {
-            "id": 2,
-            "text": "Goodnight moon",
-            "completed": true
-          }
-        ]).as('preload')
+  beforeEach(function() {
+    cy.fixture('todos/all.json').as('todos')
+  })
 
-        cy.visit('/')
-        cy.wait('@preload')
+  it('loads the page', function () {
+    cy.server()
+    // Alias the fixture data
+    cy.route('/api/todos', '@todos').as('preload')
 
-        cy.store('example.test.first').should('equal', 1)
-        
-        cy.store('todos').should('deep.equal', [{
-            id: 1,
-            text: 'Hello world',
-            completed: false
-          }, {
-            id: 2,
-            text: 'Goodnight moon',
-            completed: true
-          }])
+    cy.visit('/')
+    cy.wait('@preload')
 
-        cy.get('[data-cy=todo-item-1]')
-          .should('have.text', 'Hello world')
-          .should('not.have.class', 'completed')
-          .find('.toggle')
-          .should('not.be.checked')
+    cy.store('example.test.first').should('equal', 1)
 
-        cy.get('[data-cy=todo-item-2]')
-          .should('have.text', 'Goodnight moon')
-          .should('have.class', 'completed')
-          .find('.toggle')
-          .should('be.checked')
-    })
+    // Access the fixture data as this.todos
+    cy.store('todos').should('deep.equal', this.todos)
+
+    cy.get('[data-cy=todo-item-1]')
+      .should('have.text', 'Hello world')
+      .should('not.have.class', 'completed')
+      .find('.toggle')
+      .should('not.be.checked')
+
+    cy.get('[data-cy=todo-item-2]')
+      .should('have.text', 'Goodnight moon')
+      .should('have.class', 'completed')
+      .find('.toggle')
+      .should('be.checked')
+  })
 })
