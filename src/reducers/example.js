ebe5e651eec95c836a8722c5fa61234fcecac96c
brettshollenberger <brett.shollenberger@gmail.com>
2019-02-27 14:22:49 -0500

    Update

---
+++ src/reducers/example.js
@@ -0,0 +1,14 @@
+let initialState = {
+    test: {
+        first: 1
+    }
+}
+
+const example = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default example;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import todos from './todos'
+import example from './example'
 import visibilityFilter from './visibilityFilter'
 
 const rootReducer = combineReducers({
   todos,
-  visibilityFilter
+  visibilityFilter,
+  example
 })
 
 export default rootReducer
